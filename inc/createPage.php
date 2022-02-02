dbda81072bce2758315be526ae92a3066df10144
DuncanGoodare <duncangoodare@gmail.com>
2022-02-02 13:57:34 +0000

    general style improvements

--- inc/createPage.php
+++ inc/createPage.php
@@ -10,13 +10,13 @@
                     {
                         //retrieve values from form based on ID of input
                         var search = $('#search-input').val();
-                        var title = $('#update-title').val();
-                        var description = $('#update-description').val();
-                        var streetAddress = $('#update-address').val();
-                        var city = $('#update-city').val();
-                        var postcode = $('#update-postcode').val();
-                        var number = $('#update-number').val();
-                        var email = $('#update-email').val();
+                        var title = $('#create-title').val();
+                        var description = $('#create-description').val();
+                        var streetAddress = $('#create-address').val();
+                        var city = $('#create-city').val();
+                        var postcode = $('#create-postcode').val();
+                        var number = $('#create-number').val();
+                        var email = $('#create-email').val();
                         var goals = [];
 
                         //get boolean values from checkbox fields
@@ -89,55 +89,98 @@
                 );
             });
         </script> 
+        <style>
+            body{
+                font-family: 'Nunito', sans-serif;
+            }
+
+            .goalCheckboxes{
+                display: block;
+            }
+        </style>
     </head>
 
     <body>
             <h2>Please enter the information for a new event</h2>
             <div id="create">
                 <form id="create-form" method="POST">
-                <p>Title:</p> <input id="update-title" type="text" name="update-title">
-                <p>Description:</p> <input id="update-description" type="text" name="update-description">
-                <p>Street Address:</p> <input id="update-address" type="text" name="update-address">
-                <p>City:</p> <input id="update-city" type="text" name="update-city">
-                <p>Post Code:</p> <input id="update-postcode" type="text" name="update-postcode">
-                <p>Phone Number:</p> <input id="update-number" type="text" name="update-number">
-                <p>Email:</p> <input id="update-email" type="text" name="update-email">
+                <p>Title:</p> <input id="create-title" type="text" name="create-title">
+                <p>Description:</p> <textarea id="create-description" type="text" name="create-description" rows="5" cols="75"></textarea>
+                <p>Street Address:</p> <input id="create-address" type="text" name="create-address">
+                <p>City:</p> <input id="create-city" type="text" name="create-city">
+                <p>Post Code:</p> <input id="create-postcode" type="text" name="create-postcode">
+                <p>Phone Number:</p> <input id="create-number" type="text" name="create-number">
+                <p>Email:</p> <input id="create-email" type="text" name="create-email">
                 <p>Associated Goals:</p>
                 <div>
-                    <input type="checkbox" id="noPoverty" value="noPoverty">
-                    <label for="noPoverty">No Poverty</label>
-                    <input type="checkbox" id="zeroHunger" value="zeroHunger">
-                    <label for="zeroHunger">Zero Hunger</label>
-                    <input type="checkbox" id="goodHealth" value="goodHealth">
-                    <label for="goodHealth">Good Health and Well-Being</label>
-                    <input type="checkbox" id="qualityEducation" value="qualityEducation">
-                    <label for="qualityEducation">Quality Education</label>
-                    <input type="checkbox" id="genderEquality" value="genderEquality">
-                    <label for="genderEquality">Gender Equality</label>
-                    <input type="checkbox" id="cleanWater" value="cleanWater">
-                    <label for="cleanWater">Clean Water</label>
-                    <input type="checkbox" id="affordableEnergy" value="affordableEnergy">
-                    <label for="affordableEnergy">Affordable and Clean Energy</label>
-                    <input type="checkbox" id="economicGrowth" value="economicGrowth">
-                    <label for="economicGrowth">Decent Work and Economic Growth</label>
-                    <input type="checkbox" id="industryAndInfrastructure" value="industryAndInfrastructure">
-                    <label for="industryAndInfrastructure">Industry, Innovation and Infrastructure</label>
-                    <input type="checkbox" id="reducedInequalities" value="reducedInequalities">
-                    <label for="reducedInequalities">Reduced Inequalities</label>
-                    <input type="checkbox" id="sustainableCommunities" value="sustainableCommunities">
-                    <label for="sustainableCommunities">Sustainable Cities and Communities</label>
-                    <input type="checkbox" id="responsibleProduction" value="responsibleProduction">
-                    <label for="responsibleProduction">Responsible Consumption and Production</label>
-                    <input type="checkbox" id="climateAction" value="climateAction">
-                    <label for="climateAction">Climate Action</label>
-                    <input type="checkbox" id="waterLife" value="waterLife">
-                    <label for="waterLife">Life Below Water</label>
-                    <input type="checkbox" id="landLife" value="landLife">
-                    <label for="landLife">Life on Land</label>
-                    <input type="checkbox" id="peaceJustice" value="peaceJustice">
-                    <label for="peaceJustice">Peace, Justice and Strong Institutions</label>
-                    <input type="checkbox" id="partnershipGoals" value="partnershipGoals">
-                    <label for="partnershipGoals">Partnership for The Goals</label>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="noPoverty" value="noPoverty">
+                        <label for="noPoverty">No Poverty</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="zeroHunger" value="zeroHunger">
+                        <label for="zeroHunger">Zero Hunger</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="goodHealth" value="goodHealth">
+                        <label for="goodHealth">Good Health and Well-Being</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="qualityEducation" value="qualityEducation">
+                        <label for="qualityEducation">Quality Education</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="genderEquality" value="genderEquality">
+                        <label for="genderEquality">Gender Equality</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="cleanWater" value="cleanWater">
+                        <label for="cleanWater">Clean Water</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="affordableEnergy" value="affordableEnergy">
+                        <label for="affordableEnergy">Affordable and Clean Energy</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="economicGrowth" value="economicGrowth">
+                        <label for="economicGrowth">Decent Work and Economic Growth</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="industryAndInfrastructure" value="industryAndInfrastructure">
+                        <label for="industryAndInfrastructure">Industry, Innovation and Infrastructure</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="reducedInequalities" value="reducedInequalities">
+                        <label for="reducedInequalities">Reduced Inequalities</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="sustainableCommunities" value="sustainableCommunities">
+                        <label for="sustainableCommunities">Sustainable Cities and Communities</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="responsibleProduction" value="responsibleProduction">
+                        <label for="responsibleProduction">Responsible Consumption and Production</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="climateAction" value="climateAction">
+                        <label for="climateAction">Climate Action</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="waterLife" value="waterLife">
+                        <label for="waterLife">Life Below Water</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="landLife" value="landLife">
+                        <label for="landLife">Life on Land</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="peaceJustice" value="peaceJustice">
+                        <label for="peaceJustice">Peace, Justice and Strong Institutions</label>
+                    </div>
+                    <div class="goalCheckboxes">
+                        <input type="checkbox" id="partnershipGoals" value="partnershipGoals">
+                        <label for="partnershipGoals">Partnership for The Goals</label>
+                    </div>
                 </div>
                     <button id="create-button" type="button">Submit</button>
                 </form>

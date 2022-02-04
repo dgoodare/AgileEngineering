6889b1bba4390c3c674ef92c932f2b90edb3069a
EthanGordon <55386801+EthanGordon@users.noreply.github.com>
2022-02-04 13:32:59 +0000

    commenting out everything but title

--- inc/addPageQuery.php
+++ inc/addPageQuery.php
@@ -6,7 +6,7 @@
     //Form Data from AJAX
     $data = $_POST['data'];
     json_decode($data);
-    $Title = data[0].Title;
+    $Title = data[0].Title;/*
     $Description = $_POST['Description'];
     $StreetAddress = $_POST['StreetAddress'];
     $City = $_POST['City'];
@@ -15,10 +15,10 @@
     $Email = $_POST['Email'];
     $Goals = $_POST['Goals'];
     $Latitude = $_POST['Latitude'];
-    $Longitude = $_POST['Longitude'];
+    $Longitude = $_POST['Longitude'];*/
     
     //convert to correct data types
-    $Title = strval($Title);
+    $Title = strval($Title);/*
     $Description = strval($Description);
     $StreetAddress = strval($StreetAddress);
     $City = strval($City);
@@ -33,7 +33,7 @@
         $Goals[$i] = (int)$Goals[$i];
     }
 
-    
+    */
     //SQL query
     $query = "INSERT INTO Activities (Title, Description, StreetAddress, City, Postcode, PhoneNumber, Email, Latitude, Longitude,
                 noPoverty, zeroHunger, goodHealth, qualityEducation, genderEquality, cleanWater, affordableEnergy, economicGrowth, 
@@ -44,7 +44,7 @@
     //prepate statement and bind parameters
     $stmt = mysqli_prepare($db, $query);
     
-    mysqli_stmt_bind_param($stmt, 'sssssssddddddddddddddddddd', $Title,  $Description, $StreetAddress, $City, $Postcode, $Number, $Email, $Latitude, $Longitude, $Goals[0], $Goals[1], $Goals[2], $Goals[3], $Goals[4], $Goals[5], $Goals[6], $Goals[7], $Goals[8], $Goals[9], $Goals[10], $Goals[11], $Goals[12], $Goals[13], $Goals[14], $Goals[15], $Goals[16]);
+    mysqli_stmt_bind_param($stmt, 's'/*ssssssddddddddddddddddddd'*/, $Title/*,  $Description, $StreetAddress, $City, $Postcode, $Number, $Email, $Latitude, $Longitude, $Goals[0], $Goals[1], $Goals[2], $Goals[3], $Goals[4], $Goals[5], $Goals[6], $Goals[7], $Goals[8], $Goals[9], $Goals[10], $Goals[11], $Goals[12], $Goals[13], $Goals[14], $Goals[15], $Goals[16]*/);
 
     //execute query
     mysqli_stmt_execute($stmt);
